5b3ed829a5d67b460091a4a72b9b98e6807b8cc7
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-04-12 13:27:44 +1000

    Refactor CSS

--- lib.php
+++ lib.php
@@ -60,6 +60,7 @@ function print_header($title, $js = null, $extra_head_content = '')
     echo "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n";
     echo "    <meta name=\"google-site-verification\" content=\"VWE-XX0CgPTHYgUqJ6t13N75y-p_Q9dgtqXikM3EsBo\" />\n";
     echo "    <title>{$CONFIG['site_name']} | {$title}</title>\n";
+    echo '    <link rel="stylesheet" type="text/css" media="all" href="'.theme_file('css/layout.css')."\" />\n";
     echo '    <link rel="stylesheet" type="text/css" media="all" href="'.theme_file('css/stylesheet.css')."\" />\n";
     echo "     <script src=\"//code.jquery.com/jquery-latest.min.js\"></script>\n";
 
